a495c17187c2fd3b8d927c9b3b4351e71a288237
Mrinal Kalakrishnan <kalakris@usc.edu>
2012-03-05 17:07:13 -0800

    conversions: Added ros_to_tf.h

---
+++ usc_utils/conversions/include/conversions/ros_to_tf.h
@@ -0,0 +1,28 @@
+/*
+ * ros_to_tf.h
+ *
+ *  Created on: Mar 5, 2012
+ *      Author: kalakris
+ */
+
+#ifndef CONVERSIONS_ROS_TO_TF_H_
+#define CONVERSIONS_ROS_TO_TF_H_
+
+#include <tf/transform_datatypes.h>
+
+namespace conversions
+{
+  static void convert(const geometry_msgs::Pose& ros_pose,
+                      tf::Transform& tf_transform);
+
+  // inline implementations
+  static inline void convert(const geometry_msgs::Pose& ros_pose,
+                      tf::Transform& tf_transform)
+  {
+    tf::poseMsgToTF(ros_pose, tf_transform);
+  }
+
+}
+
+
+#endif /* CONVERSIONS_ROS_TO_TF_H_ */
---
+++ usc_utils/conversions/test/test_conversions.cpp
@@ -0,0 +1,19 @@
+/*
+ * test_conversions.cpp
+ *
+ *  Created on: Mar 5, 2012
+ *      Author: kalakris
+ */
+
+
+#include <conversions/kdl_to_eigen.h>
+#include <conversions/kdl_to_ros.h>
+#include <conversions/ros_to_kdl.h>
+#include <conversions/ros_to_tf.h>
+
+// just a test which includes all headers and makes sure it compiles
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
