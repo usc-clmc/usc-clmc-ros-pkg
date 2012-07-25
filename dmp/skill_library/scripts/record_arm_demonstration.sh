1d58df3bf1b46258473b4e7f1be3c3dd0894f2c4
Peter Pastor <pastorsa@usc.edu>
2012-07-25 16:06:13 -0700

    Fixed recording script

--- dmp/skill_library/scripts/record_arm_demonstration.sh
+++ dmp/skill_library/scripts/record_arm_demonstration.sh
@@ -4,4 +4,11 @@ cd `rospack find arm_dmp_data`
 mkdir -p dmp_data/demonstrations
 cd dmp_data/demonstrations
 
-rosbag record /joint_states /SL/right_arm_wrench_processed /SL/right_hand_strain_gauges -O $1
+rosbag record /joint_states \
+ /SL/r_hand_wrench_processed \
+ /SL/l_hand_wrench_processed \
+ /SL/r_hand_strain_gauges \
+ /SL/l_hand_strain_gauges \
+ /SL/r_hand_accelerations_processed \
+ /SL/l_hand_accelerations_processed \
+ -O $1
