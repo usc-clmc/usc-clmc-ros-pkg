ca7083ecb73a2b7cbde8692b9d2b5360c2240672
daniel kappler <daniel.kappler@gmail.com>
2013-08-05 16:14:07 +0200

    fixed some eigen stuff

--- planning/template_based_grasp_planning/grasp_template_planning/include/deep_learning/extract_template.h
+++ planning/template_based_grasp_planning/grasp_template_planning/include/deep_learning/extract_template.h
@@ -15,12 +15,40 @@
 #ifndef EXTRACT_TEMPLATE_H
 #define EXTRACT_TEMPLATE_H
 
-class Extract_template{
+#include <Eigen/Eigen>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+
+typedef boost::mt19937 base_generator_type;
+
+class Extract_template
+{
 private:
+  boost::variate_generator<base_generator_type&, boost::uniform_real<> > _uni;
+  Eigen::Vector3d _bounding_box_corner_1;
+  Eigen::Vector3d _bounding_box_corner_2;
+
+  Extract_template():
+  _uni(generator(std::time(0),boost::uniform_real<> uni_dist(0,1))),_bounding_box_corner_1(bounding_box_corner_1),
+  {
+  }
+  ;Extract_template(Extract_template &extract_template):
+  _uni(generator(std::time(0),boost::uniform_real<> uni_dist(0,1))),_bounding_box_corner_1(bounding_box_corner_1),
+  {
+  }
+  ;
 protected:
 public:
-	Extract_template();
-	virtual ~Extract_template(){};
+  Extract_template(Eigen::Vector3d bounding_box_corner_1, Eigen::Vector3d bounding_box_corner_2);
+  virtual ~Extract_template()
+  {
+  }
+  ;
+
+  void Get_random_grasp_templates(
+      grasp_template::GraspTemplate &g_temp,
+      std::vector<grasp_template::GraspTemplate, Eigen::aligned_allocator<grasp_template::GraspTemplate> > &result_template);
+
 };
 
 #endif /*EXTRACT_TEMPLATE_H*/
--- planning/template_based_grasp_planning/grasp_template_planning/src/deep_learning/extract_template.cpp
+++ planning/template_based_grasp_planning/grasp_template_planning/src/deep_learning/extract_template.cpp
@@ -14,16 +14,27 @@
 
 #include <deep_learning/extract_template.h>
 
-Extract_template::Extract_template()
+#include <ctime>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+
+Extract_template::Extract_template(Eigen::Vector3d bounding_box_corner_1, Eigen::Vector3d bounding_box_corner_2)
+ _uni(generator(std::time(0),boost::uniform_real<> uni_dist(0,1))),_bounding_box_corner_1(bounding_box_corner_1),
+ _bounding_box_corner_2(bounding_box_corner_2)
 {
+  // have to rescale the random stuff
+
 
 }
-void Extract_random_grasp_templates(
+
+void Get_random_grasp_templates(
     grasp_template::GraspTemplate &g_temp,
     std::vector<grasp_template::GraspTemplate, Eigen::aligned_allocator<grasp_template::GraspTemplate> > &result_template)
 {
   result_template.clear();
 
+  double x, y, z;
+  double roll, pitch, yaw;
   for (int i = 0; i < _max_samples; ++i)
   {
     gras_template::GraspTemplate local_grasp_template(g_temp);
@@ -35,6 +46,16 @@ void Extract_random_grasp_templates(
     // transform this in the template frame and then
     // sample from the applied grasps
     // this is required to actually compute the mask
+
+    // sample x,y,z
+
+    // sample roll,pitch,yaw
+
+    // I can use roll pitch yaw since I will only sample small rotations
+    // such that a gimbal lock will most likely not occur
+    Eigen::Quaterniond sample_orientation;
+    sample_orientation *= Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitZ())
+        * Eigen::AngleAxisf(pitch, Eigen::Vector3f::UnitY()) * Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ());
 
     // heightmap is not important
     grasp_template::DismatchMeasure d_measure(local_grasp_template.heightmap_, identity_pose, sampled_gripper_pose);
